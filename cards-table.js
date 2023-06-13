6006804c13cbe3c9399fc8b5483a7e6dffc87254
h0oliganka <pingvinchik41@gmail.com>
2023-06-13 15:26:32 +0300

    карты grid

--- cards-table.js
+++ cards-table.js
@@ -1,43 +1,41 @@
 import { cards } from './index.js'
 
 export function cardsTableGenerate(value) {
-    const randomCards = []
+    const randomCards = [];
     for (let i = 0; i < value / 2; i++) {
         randomCards.push(
             cards[Math.round(0 - 0.5 + Math.random() * (35 - 0 + 1))]
-        )
+        );
     }
     const gameCardSet = randomCards
         .concat(randomCards)
         .sort(() => 0.5 - Math.random())
     console.log(gameCardSet)
-    const cardsTable = gameCardSet
-        .map((card) => {
-            let suit
-            switch (card.charAt(0)) {
-                case 'П':
-                    suit = './img/piki.svg'
-                    break
-                case 'Ч':
-                    suit = './img/cher.svg'
-                    break
-                case 'Б':
-                    suit = './img/bubna.svg'
-                    break
-                case 'К':
-                    suit = './img/chrest.svg'
-                    break
-                default:
-                    break
-            }
-            return `<div class="card">
+    const cardsTable = gameCardSet.map((card) => {
+        let suit;
+        switch (card.charAt(0)) {
+            case 'П':
+                suit = './img/piki.svg'
+                break
+            case 'Ч':
+                suit = './img/cher.svg'
+                break
+            case 'Б':
+                suit = './img/bubna.svg'
+                break
+            case 'К':
+                suit = './img/chrest.svg'
+                break
+            default:
+                break;
+        }
+        return `<div class="card" data-cardValue='${card}'>
             <p class="card-value-up">${card.slice(1)}</p>
             <p class="card-value-down">${card.slice(1)}</p>
             <img src="${suit}" alt="pik" class="suit-big">
             <img src="${suit}" alt="pik" class="suit-small-up">
             <img src="${suit}" alt="pik" class="suit-small-down">
-        </div>`
-        })
-        .join('')
-    return cardsTable
+        </div>`;
+    });
+    return cardsTable;
 }
--- level-render.js
+++ level-render.js
@@ -3,20 +3,43 @@ import { headerRenderer } from './header.js'
 
 export function levelPageRenderer({ app }) {
     const appHtml = `<div class="header-component"></div>
-    <div class="card-field"> </div>
+    <div class="card-field grid_${localStorage.getItem('Level')}"> </div>
 `
     let backSideCards = []
     for (let i = 0; i < 6 * Number(localStorage.getItem('Level')); i++) {
-        backSideCards.push('<img src="./img/рубашка.svg" alt="card-backside">')
+        backSideCards.push(`<div class="card" data-id='${i}'><img class="card-back" src="./img/рубашка.svg" alt="card-backside"></div>`)
     }
     console.log(backSideCards)
     app.innerHTML = appHtml
-    document.querySelector('.card-field').innerHTML = cardsTableGenerate(backSideCards.length)
-    setTimeout(
-        () =>
-            (document.querySelector('.card-field').innerHTML =
-            (backSideCards.join(''))),
-        5000
-    )
+    const cardTable = cardsTableGenerate(backSideCards.length);
+    document.querySelector('.card_field').innerHTML = cardTable.join('');
+
+    setTimeout(() => {
+        document.querySelector('.card_field').innerHTML =
+            backSideCards.join('');
+        let clickedCard = [];
+        for (const card of document.querySelectorAll('.card')) {
+            card.addEventListener('click', (event) => {
+                event.stopPropagation();
+                clickedCard.push(cardTable[card.dataset.id]);
+                let targetCard = document.createElement('div');
+                targetCard.innerHTML = cardTable[card.dataset.id];
+                card.replaceWith(targetCard);
+                if (clickedCard.length === 2) {
+                    if (clickedCard[0] === clickedCard[1]) {
+                        setTimeout(function () {
+                            alert('Вы победили!');
+                        }, 100);
+                        clickedCard = [];
+                    } else {
+                        setTimeout(function () {
+                            alert('Вы проиграли!');
+                        }, 100);
+                        clickedCard = [];
+                    }
+                }
+            });
+        }
+    }, 5000);
     headerRenderer({ element: document.querySelector('.header-component') })
 }
