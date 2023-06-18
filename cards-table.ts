141be776382b266d00eb407c9e193d271a1fce4d
Daria Salova <pingvinchik41@gmail.com>
2023-06-18 19:42:56 +0300

    исправление ошибок в ts

--- cards-table.ts
+++ cards-table.ts
@@ -1,18 +1,18 @@
 import { cards } from './index'
 
 export function cardsTableGenerate(value: number) {
-    const randomCards = [];
+    const randomCards: string[] = []
     for (let i = 0; i < value / 2; i++) {
         randomCards.push(
             cards[Math.round(0 - 0.5 + Math.random() * (35 - 0 + 1))]
-        );
+        )
     }
     const gameCardSet = randomCards
         .concat(randomCards)
         .sort(() => 0.5 - Math.random())
     console.log(gameCardSet)
-    const cardsTable = gameCardSet.map((card) => {
-        let suit;
+    const cardsTable = gameCardSet.map((card: string) => {
+        let suit
         switch (card.charAt(0)) {
             case 'П':
                 suit = './img/piki.svg'
@@ -27,7 +27,7 @@ export function cardsTableGenerate(value: number) {
                 suit = './img/chrest.svg'
                 break
             default:
-                break;
+                break
         }
         return `<div class="card" data-cardValue='${card}'>
             <p class="card-value-up">${card.slice(1)}</p>
@@ -35,7 +35,7 @@ export function cardsTableGenerate(value: number) {
             <img src="${suit}" alt="pik" class="suit-big">
             <img src="${suit}" alt="pik" class="suit-small-up">
             <img src="${suit}" alt="pik" class="suit-small-down">
-        </div>`;
-    });
-    return cardsTable;
+        </div>`
+    })
+    return cardsTable
 }
