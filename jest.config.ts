dcc39c4197bdddf102009f03fb6e810c7be8a147
h0oliganka <pingvinchik41@gmail.com>
2023-06-28 18:15:26 +0300

    тесты

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleNameMapper: { '^.+\\.(css|less)$': '<rootDir>/CSSStub.js' },
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: { '^.+\\.(css|less)$': '<rootDir>/CSSStub.js' },
+};
